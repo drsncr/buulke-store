897b0891ca0a3db66eca4b6c174b5485d53db49e
E.P Selek <epselek@gmail.com>
2020-03-01 22:23:11 +0300

    initial commit

---
+++ src/main/java/com/buulke/buulkestore/BuulkeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.buulke.buulkestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuulkeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuulkeStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buulke/buulkestore/BuulkeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buulke.buulkestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuulkeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c5fb852b326a237dda7de239b2c2267c14c9af67
drsncr <dursunacarr@gmail.com>
2020-03-03 06:56:33 +0300

    object mapper added to convert data to question class

--- src/main/java/com/buulke/buulkestore/service/RabbitMqListener.java
+++ src/main/java/com/buulke/buulkestore/service/RabbitMqListener.java
@@ -1,6 +1,9 @@
 package com.buulke.buulkestore.service;
 
 import com.buulke.buulkestore.model.Question;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -8,8 +11,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitMqListener {
 
-    @RabbitListener(queues = "questionQueue")
-    public void handleMessage(@Payload Question question) {
-        System.err.println(question);
+    @RabbitListener(queues = "questionQueue1")
+    public void handleMessage(@Payload String questionMessage) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Queu
+        try {
+            Question question = objectMapper.readValue(questionMessage, Question.class);
+            System.out.println(question);
+        }catch (JsonProcessingException ex){
+            System.out.println("questionMessage can not be parsed => " + ex.getMessage());
+        }
     }
 }
